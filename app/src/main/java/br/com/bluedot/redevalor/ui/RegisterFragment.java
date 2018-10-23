1242856a113699e0d2376104aa1d9d3a1a9c0834
Matheus Souza <matheus.souza@zup.com.br>
2018-10-23 10:15:42 -0300

    Adhrence and Login Views Done

--- app/src/main/java/br/com/bluedot/redevalor/ui/RegisterFragment.java
+++ app/src/main/java/br/com/bluedot/redevalor/ui/RegisterFragment.java
@@ -1,14 +1,23 @@
 package br.com.bluedot.redevalor.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import br.com.bluedot.redevalor.R;
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class RegisterFragment extends BaseFragment {
+
+    @BindView(R.id.spFunction)
+    Spinner mSpinner;
 
     public static final String TAG = RegisterFragment.class.getSimpleName();
 
@@ -20,5 +29,23 @@ public class RegisterFragment extends BaseFragment {
     @Override
     public View onCreateViewBinder(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initSpinner();
+    }
+
+    private void initSpinner() {
+        ArrayAdapter<String> spinnerCountShoesArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.function_array));
+        mSpinner.setAdapter(spinnerCountShoesArrayAdapter);
+    }
+
+    @OnClick(R.id.btLogin)
+    public void onClickLogin() {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
     }
 }
